3f272fe99b9672bc247d6ce4e3a6246a10aae501
ssrag <ssrag@SSRamakrishna>
2023-02-08 16:52:53 +0530

    update

--- Base_Framework_ELF/src/main/java/generic/UtilityMethods.java
+++ Base_Framework_ELF/src/main/java/generic/UtilityMethods.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Properties;
 import java.util.Set;
-
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -14,20 +13,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.io.FileHandler;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
-
 
 /**
  * 
  * @author Yogita
  *
  */
-public class UtilityMethods implements FrameworkConstants  {
-	
+public class UtilityMethods implements FrameworkConstants {
+
 	public static WebDriver driver;
 	public static ExtentReports report;
 	public static ExtentTest test;
@@ -37,28 +34,27 @@ public class UtilityMethods implements FrameworkConstants  {
 	public static JavascriptExecutor js;
 	public static Actions action;
 
-	
-	/*Initialize objects of JS, actions, */
+	/* Initialize objects of JS, actions, */
 	public void initObjects() {
-		
+
 		js = (JavascriptExecutor) driver;
 		action = new Actions(driver);
-				
+
 	}
 
 	public static String name() {
-		LocalDateTime sysdate=LocalDateTime.now();
-		String name=sysdate.toString().replace(":", "-");
+		LocalDateTime sysdate = LocalDateTime.now();
+		String name = sysdate.toString().replace(":", "-");
 		return name;
 	}
-	
+
 	public String get_title() {
-		String title= driver.getTitle();
-		 return title;
+		String title = driver.getTitle();
+		return title;
 	}
 
-	public String get_url() {		
-		String CurrentURL=driver.getCurrentUrl();
+	public String get_url() {
+		String CurrentURL = driver.getCurrentUrl();
 		return CurrentURL;
 	}
 
@@ -66,58 +62,62 @@ public class UtilityMethods implements FrameworkConstants  {
 		element_To_Click.click();
 	}
 
-	public void enter_value(WebElement TextField,String value) {		
+	public void enter_value(WebElement TextField, String value) {
 		TextField.sendKeys(value);
 	}
 
-	
-	public void switchToASpecificTitledWindow(String title) { 
-		String parentWindowId = driver.getWindowHandle(); 
+	public void switchToASpecificTitledWindow(String title) {
+		String parentWindowId = driver.getWindowHandle();
 
-		Set<String> allWindowIds = driver.getWindowHandles(); allWindowIds.remove(parentWindowId); 
-		for(String windowId:allWindowIds){ driver.switchTo().window(windowId);
-		if(driver.getTitle().equalsIgnoreCase(title)){ 
-			break; 
-		} 
-		} 
-	} 
+		Set<String> allWindowIds = driver.getWindowHandles();
+		allWindowIds.remove(parentWindowId);
+		for (String windowId : allWindowIds) {
+			driver.switchTo().window(windowId);
+			if (driver.getTitle().equalsIgnoreCase(title)) {
+				break;
+			}
+		}
+	}
+
 	public void switchToASpecificWindow(WebElement element) {
 
-		String parent=driver.getWindowHandle(); 
-		Set<String> allWid=driver.getWindowHandles(); 
-		allWid.remove(parent); 
-		for(String sessionID:allWid){ 
+		String parent = driver.getWindowHandle();
+		Set<String> allWid = driver.getWindowHandles();
+		allWid.remove(parent);
+		for (String sessionID : allWid) {
 			driver.switchTo().window(sessionID);
 
-			if(element.isDisplayed()){ 
-				break; 
-			} 
-		} 
+			if (element.isDisplayed()) {
+				break;
+			}
+		}
 	}
+
 	public static String getPhoto(WebDriver driver) {
 
-		String imgPath=SCREENSHOT_PATH+name()+".png";
-		TakesScreenshot takeScreenShot=(TakesScreenshot)driver;
-		File source=takeScreenShot.getScreenshotAs(OutputType.FILE);
-		File destination= new File(imgPath);
+		String imgPath = SCREENSHOT_PATH + name() + ".png";
+		TakesScreenshot takeScreenShot = (TakesScreenshot) driver;
+		File source = takeScreenShot.getScreenshotAs(OutputType.FILE);
+		File destination = new File(imgPath);
 		try {
 			FileHandler.copy(source, destination);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return "."+imgPath;
+		return "." + imgPath;
 	}
-	
+
 	/**
 	 * This method read the properties and return the value based on key
+	 * 
 	 * @param key
 	 * @return value
 	 * @throws Throwable
 	 */
-	public String  getValueProperty(String key) throws Throwable {
-		FileInputStream file=new FileInputStream(PROPERTIES_PATH);
-		Properties properties=new Properties();
+	public String getValueProperty(String key) throws Throwable {
+		FileInputStream file = new FileInputStream(PROPERTIES_PATH);
+		Properties properties = new Properties();
 		properties.load(file);
 		return properties.getProperty(key);
 	}
--- Base_Framework_ELF/src/main/java/pom_scripts/WelcomePage.java
+++ Base_Framework_ELF/src/main/java/pom_scripts/WelcomePage.java
@@ -4,22 +4,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class WelcomePage extends BasePage{
-	
-	public WelcomePage(WebDriver driver) {
+public class WelcomePage extends BasePage 
+{
+
+	public WelcomePage(WebDriver driver) 
+	{
 		super(driver);
 	}
-	
+
 	@FindBy(className = "ico-login")
 	WebElement Loginlink;
-	
+
 	public void clickLogin() {
 		clickAction(Loginlink);
 	}
-	
+
 	@FindBy(className = "ico-register")
 	WebElement RegisterLink;
-	
+
 	public void clickRegister() {
 		clickAction(RegisterLink);
 	}
